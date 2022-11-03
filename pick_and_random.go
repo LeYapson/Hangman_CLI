b5d91b925f91998e93c76d93c5198dad93829f4c
Théau Yapi <leyapson@pop-os.localdomain>
2022-11-03 12:46:10 +0100

    sa veut pas blind

--- pick_and_random.go
+++ pick_and_random.go
@@ -4,18 +4,42 @@ import (
 	"fmt"
 	"math/rand"
 	"time"
-	//"github.com/01-edu/z01"
+
+	"github.com/01-edu/z01"
 )
 
-func PickandRandom() {
+func PickandRandom() string {
 	// Seeding with the same value results in the same random sequence each run.
 	// For different numbers, seed with a different value, such as
 	// time.Now().UnixNano(), which yields a constantly-changing number.
 	rand.Seed(time.Now().UnixNano())
 	wordtofind := ImportTxt()
 	word := (wordtofind[rand.Intn(len(wordtofind))])
-	Blind(word)
-	//n := len(wordtofind) / 2 - 1
+	array := []rune(word)
+	for i := 0; i < len(array); i++{
+		if array[i] >= 'a' && array[i] <= 'z' {
+			
+			array[i] = '_'
+			
+		}
+		fmt.Print(LettertoShow(word))
+		z01.PrintRune(array[i])
+	}
+	return string(array)
 	
-fmt.Println(Blind(word))
+	
+	
+	
+
+}
+
+func LettertoShow(s string) string {
+	array := []rune(s)
+	n := len(array) / 2 - 1
+
+	array = append(array, rune(n))
+
+
+	return string(array)
+
 }
