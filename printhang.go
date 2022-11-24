3beb9482285b1766722dc34d0fee68a4a1451690
theau <yapsonstudio@gmail.com>
2022-11-24 13:58:46 +0100

    je commit depuis mon streamdeck donc le nom changera pas beaucoup

--- printhang.go
+++ printhang.go
@@ -2,116 +2,50 @@ package main
 
 import "fmt"
 
-func printhang0() { // coordonnées du hangman
-	hang := ImportHang()
-
-	for i := 1; i <= 7; i++ {
-		fmt.Println(hang[i])
-	}
-
-}
-
-func printhang1() {
-	hang := ImportHang() //*
-
-	for i := 7; i < 15; i++ {
-		fmt.Println(hang[i])
-	}
-
-}
-
-func printhang2() { //*
-	hang := ImportHang()
-
-	for i := 15; i < 23; i++ {
-		fmt.Println(hang[i])
-	}
-
-}
-
-func printhang3() { //*
-	hang := ImportHang()
-
-	for i := 23; i < 31; i++ {
-		fmt.Println(hang[i])
-	}
-
-}
-
-func printhang4() { //*
-	hang := ImportHang()
-
-	for i := 31; i < 39; i++ {
-		fmt.Println(hang[i])
-	}
-
-}
-
-func printhang5() { //*
-	hang := ImportHang()
-
-	for i := 39; i < 47; i++ {
-		fmt.Println(hang[i])
-	}
-
-}
-
-func printhang6() { //*
-	hang := ImportHang()
-
-	for i := 47; i < 55; i++ {
-		fmt.Println(hang[i])
-	}
-
-}
-
-func printhang7() { //*
-	hang := ImportHang()
-
-	for i := 55; i < 63; i++ {
-		fmt.Println(hang[i])
-	}
-
-}
-
-func printhang8() { //*
-	hang := ImportHang()
-
-	for i := 63; i < 71; i++ {
-		fmt.Println(hang[i])
-	}
-
-}
-
-func printhang9() { //*
-	hang := ImportHang()
-
-	for i := 71; i < 79; i++ {
-		fmt.Println(hang[i])
-	}
-
-}
 func PrintHang(n int) {
+	hang := ImportHang()
+
 	if n == 9 {
-		printhang0()
+		for i := 1; i <= 7; i++ {
+			fmt.Println(hang[i])
+		}
 	} else if n == 8 {
-		printhang1()
+		for i := 7; i < 15; i++ {
+			fmt.Println(hang[i])
+		}
 	} else if n == 7 {
-		printhang2()
+		for i := 15; i < 23; i++ {
+			fmt.Println(hang[i])
+		}
+
 	} else if n == 6 {
-		printhang3()
+		for i := 23; i < 31; i++ {
+			fmt.Println(hang[i])
+		}
 	} else if n == 5 {
-		printhang4()
+		for i := 31; i < 39; i++ {
+			fmt.Println(hang[i])
+		}
 	} else if n == 4 {
-		printhang5()
+		for i := 39; i < 47; i++ {
+			fmt.Println(hang[i])
+		}
 	} else if n == 3 {
-		printhang6()
+		for i := 47; i < 55; i++ {
+			fmt.Println(hang[i])
+		}
 	} else if n == 2 {
-		printhang7()
+		for i := 55; i < 63; i++ {
+			fmt.Println(hang[i])
+		}
 	} else if n == 1 {
-		printhang8()
+		for i := 63; i < 71; i++ {
+			fmt.Println(hang[i])
+		}
 	} else if n == 0 {
-		printhang9()
+		for i := 71; i < 79; i++ {
+			fmt.Println(hang[i])
+		}
 	}
 
 }
